8340f84b7d047791d452af18b9f0bccc72f18528
Rodrigo Mendes Nunes <rmn.dev.mobility@gmail.com>
2017-04-01 09:32:38 -0300

    commit 2

--- app/src/main/java/ddm/fatec/com/br/appinitialgit/MainActivity.java
+++ app/src/main/java/ddm/fatec/com/br/appinitialgit/MainActivity.java
@@ -9,12 +9,14 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Context context;
-    private Button botao1;
-    private Button botao2;
-    private Button botao3;
-    private Button botao4;
-    private Button botao5;
-    private Button botao6;
+    private Button botaoTuri;
+    private Button botaoGabi;
+    private Button botaoGui;
+    private Button botaoJulio;
+    private Button botaoDalfito;
+    private Button botaoToniolli;
+    private Button botaoMayara;
+    private Button botaoSamuel;
     private Button botaoProf;
 
 
@@ -27,12 +29,14 @@ public class MainActivity extends AppCompatActivity {
 
         botaoProf = (Button) findViewById(R.id.btnProf);
 
-        botao1 = (Button) findViewById(R.id.btnTuri);
-        botao2 = (Button) findViewById(R.id.btnGabi);
-        botao3 = (Button) findViewById(R.id.btnGui);
-        botao4 = (Button) findViewById(R.id.btnJulio);
-        botao5 = (Button) findViewById(R.id.btnDalfito);
-        botao6 = (Button) findViewById(R.id.btnSamuel);
+        botaoTuri = (Button) findViewById(R.id.btnTuri);
+        botaoGabi = (Button) findViewById(R.id.btnGabi);
+        botaoGui = (Button) findViewById(R.id.btnGui);
+        botaoJulio = (Button) findViewById(R.id.btnJulio);
+        botaoDalfito = (Button) findViewById(R.id.btnDalfito);
+        botaoDalfito = (Button) findViewById(R.id.btnToniolli);
+        botaoDalfito = (Button) findViewById(R.id.btnMayara);
+        botaoSamuel = (Button) findViewById(R.id.btnSamuel);
 
         botaoProf.setOnClickListener(new View.OnClickListener() {
             @Override

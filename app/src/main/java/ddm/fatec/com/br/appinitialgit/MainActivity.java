99731ba764a346ee80ae0c09d2e0f0e5a3ba3703
0200831421001 <gaby_.dc@hotmail.com>
2017-04-01 11:01:45 -0300

    navegacaoogabi

--- app/src/main/java/ddm/fatec/com/br/appinitialgit/MainActivity.java
+++ app/src/main/java/ddm/fatec/com/br/appinitialgit/MainActivity.java
@@ -1,10 +1,13 @@
 package ddm.fatec.com.br.appinitialgit;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import static android.R.attr.label;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        botaoGabi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent novaTela = new Intent(context, gabiActivity.class);
+                startActivity(novaTela);
+            }
+        });
     }
 }
